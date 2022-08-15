d1fa9b4541cfc5eb47e1382e212c3b10924de3ea
Yahav Sabag <yahavsabag9@gmail.com>
2022-08-15 04:30:35 +0300

    routes are at it again

---
+++ src/comp/PageNotFound.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const PageNotFound = () => {
+    console.log(window.location)
+    console.log(window.location.pathname)
+  return (
+    <div>PageNotFound
+    <>
+    <h1>404</h1>
+    </>
+    </div>
+  )
+}
+
+export default PageNotFound
